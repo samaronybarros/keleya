75d90d15f8df42ca4425ba3c2cdacc45e7be3d6f
Sam Barros <samarony.barros@gmail.com>
2019-02-19 19:39:16 +0100

    Includes bodyParse and CORS

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 require('dotenv').load()
 
 import * as express from 'express'
+import * as cors from 'cors'
+import * as bodyParser from 'body-parser'
 
 const config = require('./config')
 
 const app = express()
 const port = config.serverPort || 3000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 
 app.get('/', (req, res, next) => {
     res.json('Hello Keleya')
