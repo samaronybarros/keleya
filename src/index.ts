e7ceeb962f57f9d92e46ef1c860d140830295cd6
Sam Barros <samarony.barros@gmail.com>
2019-02-19 20:11:54 +0100

    Updates index.ts to set up routes. Phew! It works :)

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ require('dotenv').load()
 import * as express from 'express'
 import * as cors from 'cors'
 import * as bodyParser from 'body-parser'
+import { userRouter } from './routers/user.router'
+import { tokenGuard } from './middlewares/token-guard'
 
 const config = require('./config')
 
@@ -12,9 +14,17 @@ const port = config.serverPort || 3000
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
+app.use('/', userRouter)
 
-app.get('/', (req, res, next) => {
-    res.json('Hello Keleya')
+// Unprotected Get
+app.get('/some-resource', (req, res, next) => {
+    res.json('Hello World')
+})
+app.use(tokenGuard())
+
+// Protected Get
+app.get('/some-protected-resource', (req, res, next) => {
+    res.json('Protected Hello World')
 })
 
 app.listen(port, () => {
