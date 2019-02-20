5fa0706b774a6e61febcd93dd8d9975212614a6b
Sam Barros <samarony.barros@gmail.com>
2019-02-20 18:52:51 +0100

    Creates Post Model

---
+++ src/models/post.ts
@@ -0,0 +1,47 @@
+import * as Sequelize from 'sequelize'
+import { sequelize } from '../instances/sequelize'
+import { User } from './user'
+
+export interface PostAddModel {
+    author_id: number
+    title: string
+    body: string
+}
+
+export interface PostLimitModel {
+    limit: number
+}
+
+export interface PostModel extends Sequelize.Model<PostModel, PostAddModel> {
+    id: number
+    author_id: number
+    title: string
+    body: string
+    createdAt: string
+    updatedAt: string
+}
+
+export interface PostViewModel {
+    id: number
+    title: string
+    body: string
+    author_id: number
+}
+
+export const Post = sequelize.define<PostModel, PostAddModel>('posts', {
+    author_id: {
+        type: Sequelize.INTEGER,
+        references: {
+            model: 'users',
+            key: 'id',
+        },
+    },
+    title: Sequelize.STRING,
+    body: Sequelize.STRING,
+})
+
+Post.belongsTo(User, {
+    as: 'author',
+    onDelete: 'CASCADE',
+    foreignKey: { name: 'author_id', allowNull: false },
+})
