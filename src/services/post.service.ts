d64b2f29ddd9bdbca1708e0e911ecbf833d222b8
Sam Barros <samarony.barros@gmail.com>
2019-02-20 19:03:16 +0100

    Creates Post Service

---
+++ src/services/post.service.ts
@@ -0,0 +1,43 @@
+import * as Bluebird from 'bluebird'
+import * as Sequelize from 'sequelize'
+import { Post, PostAddModel, PostViewModel, PostLimitModel } from '../models/post'
+import { User } from '../models/user'
+
+export class PostService {
+    static get postAttributes() {
+        return ['id', 'title', 'body']
+    }
+    static get userAttributes() {
+        return ['id', 'username']
+    }
+    private static _post
+    static get post() {
+        return PostService._post
+    }
+
+    register({ title, body, author_id }: PostAddModel) {
+        return Post.create({ title, body, author_id }).then(p => this.getPostById(p!.id))
+    }
+
+    getPostById(id: number) {
+        return Post.findById(id, {
+            attributes: PostService.postAttributes,
+        }) as Bluebird<PostViewModel>
+    }
+
+    getPosts({ limit }: PostLimitModel) {
+        return Post.findAll({
+            include: [
+                {
+                    model: User,
+                    where: { author_id: Sequelize.col('author.id') },
+                    attributes: PostService.userAttributes,
+                    as: 'author',
+                },
+            ],
+            attributes: PostService.postAttributes,
+            limit: limit ? limit : 10,
+            order: [['createdAt', 'DESC']],
+        })
+    }
+}
